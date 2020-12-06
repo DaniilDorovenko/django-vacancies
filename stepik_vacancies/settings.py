bb6cee563fcef8a9dd173fbe7d637a6e811a9f8f
Daniil Dorovenko <dorovenco@mail.ru>
2020-12-06 15:53:28 +0300

    static

--- stepik_vacancies/settings.py
+++ stepik_vacancies/settings.py
@@ -132,7 +132,7 @@ DATE_FORMAT = 'd E'
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
